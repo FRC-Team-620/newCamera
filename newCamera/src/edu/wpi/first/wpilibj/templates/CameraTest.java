bc9af03587f771ddda85d09d495961c38d0c1246
Aaron Walsh <ledzeppelindrummer@gmail.com>
2013-01-26 11:33:33 -0500

    Restructured code into a class!
    Now usable in another project.
    Expanable (NOTE: UNTESTED)

--- newCamera/src/edu/wpi/first/wpilibj/templates/CameraTest.java
+++ newCamera/src/edu/wpi/first/wpilibj/templates/CameraTest.java
@@ -36,76 +36,20 @@ import edu.wpi.first.wpilibj.image.RGBImage;
  * chain (open it with the Vision Assistant)
  */
 public class CameraTest extends SimpleRobot {
-    
-    AxisCamera camera;          // the axis camera object (connected to the switch)
-    CriteriaCollection cc;      // the criteria for doing the particle filter operation
-    
+     cameraController cam = null;
     public void robotInit() {
-        camera = AxisCamera.getInstance();  // get an instance ofthe camera
-        cc = new CriteriaCollection();      // create the criteria for the particle filter
-        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 10, 800, false);
-        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 10, 800, false);
+    cam = new cameraController(AxisCamera.getInstance());
     }
 
     public void autonomous() {
         while (isAutonomous() && isEnabled()) {
-            try {
-                /**
-                 * Do the image capture with the camera and apply the algorithm described above. This
-                 * sample will either get images from the camera or from an image file stored in the top
-                 * level directory in the flash memory on the cRIO. The file name in this case is "10ft2.jpg"
-                 * 
-                 */
-                ColorImage image;     
-                    image = camera.getImage(); // comment if using stored image
-                //ColorImage image;                           // next 2 lines read image from flash on cRIO
-                //image =  new RGBImage("/10ft2.jpg");
-                BinaryImage thresholdImage = image.thresholdRGB(0, 45, 100, 255, 0, 45);   // keep only red objects
-              //BinaryImage thresholdImage2 = image.thresholdRGB(200, 255, 100, 255, 200, 255);   // keep only red objects
-                BinaryImage bigObjectsImage = thresholdImage.removeSmallObjects(false, 2);  // remove small artifacts tweak erosion amount
-              //BinaryImage bigObjectsImage2 = thresholdImage2.removeSmallObjects(false, 2);  // remove small artifacts
-                BinaryImage convexHullImage = bigObjectsImage.convexHull(false);          // fill in occluded rectangles
-                BinaryImage filteredImage = convexHullImage.particleFilter(cc);           // find filled in rectangles
-                
-                ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
-                int temp = 0;
-                for (int i = 0; i < reports.length; i++) {                                // print results
-                    ParticleAnalysisReport r = reports[i];
-                    double max = 0;
-                    
-                    
-                        if(reports[i].particleArea > max){
-                            max = reports[i].particleArea;
-                            temp = i;
-                        }      
-                   
-                }
-                
-                System.out.println("Particle: " + temp + ":  Center of mass x: " +
-                            reports[temp].center_mass_x + ": Center of mass y:" + 
-                            reports[temp].center_mass_y + "Size: " + reports[temp].particleArea);
-                System.out.println(filteredImage.getNumberParticles() + "  " + Timer.getFPGATimestamp());
-                
-                /**
-                 * all images in Java must be freed after they are used since they are allocated out
-                 * of C data structures. Not calling free() will cause the memory to accumulate over
-                 * each pass of this loop.
-                 */
-                filteredImage.free();
-                convexHullImage.free();
-                bigObjectsImage.free();
-              //bigObjectsImage2.free();
-                thresholdImage.free();
-              //thresholdImage2.free();
-                image.free();
-                
-          //} catch (AxisCameraException ex) {        // this is needed if the camera.getImage() is called
-          //    ex.printStackTrace();
-            } catch (NIVisionException ex) {
-                ex.printStackTrace();
-             } catch (AxisCameraException ex) {
-                    ex.printStackTrace();
-             }
+            ParticleAnalysisReport r = cam.getLargestParticle(0,45,100,255,0,45);
+           System.out.println("Particle at: "  +
+                   r.center_mass_x
+                   + "," + 
+                   r.center_mass_y +
+                   " Particle Area: " +
+                   r.particleArea);
         }
     }
 
@@ -113,10 +57,10 @@ public class CameraTest extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-        while (isOperatorControl() && isEnabled()) {
-            Timer.delay(1);
-            System.out.println(camera.getBrightness());
-        }
+//        while (isOperatorControl() && isEnabled()) {
+//            Timer.delay(1);
+//            System.out.println(camera.getBrightness());
+//        }
     }
 }
         
---
+++ newCamera/src/edu/wpi/first/wpilibj/templates/cameraController.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.camera.AxisCameraException;
+import edu.wpi.first.wpilibj.image.BinaryImage;
+import edu.wpi.first.wpilibj.image.ColorImage;
+import edu.wpi.first.wpilibj.image.CriteriaCollection;
+import edu.wpi.first.wpilibj.image.NIVision.MeasurementType;
+import edu.wpi.first.wpilibj.image.NIVisionException;
+import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
+/**
+ *
+ * @author Warbots
+ */
+public class cameraController {
+  AxisCamera cam = null;
+ CriteriaCollection cc = new CriteriaCollection();      // create the criteria for the particle filter
+  
+      public cameraController(AxisCamera camera) {
+           cam = camera;
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 10, 800, false); //looks for cetian size rectangles
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 10, 800, false);
+       }
+       
+       public cameraController(AxisCamera camera,
+               int minWidth, int maxWidth, int minHeight, int maxHeight) {
+        cam = camera;
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, minWidth, maxWidth, false); //looks for cetian size rectangles
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, minHeight, maxHeight, false);
+       }
+       
+       public ParticleAnalysisReport[] getParticles(int redMin, int redMax, //returns a list of particle reports form the camera
+               int greenMin, int greenMax, 
+               int blueMin, int blueMax) {   
+        try {
+                ColorImage image;     
+                    image = cam.getImage();
+                BinaryImage thresholdImage = image.thresholdRGB(redMin, redMax, greenMin, greenMax, blueMin, blueMax);   // keep certian colors
+                BinaryImage bigObjectsImage = thresholdImage.removeSmallObjects(false, 2);  // remove small artifacts; tweak erosion amount
+                BinaryImage convexHullImage = bigObjectsImage.convexHull(false);          // fill in occluded rectangles
+                BinaryImage filteredImage = convexHullImage.particleFilter(cc);           // find filled in rectangles
+                
+                ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
+            
+                filteredImage.free();
+                convexHullImage.free();
+                bigObjectsImage.free();//THIS MUST BE DONE FOR MEM REASONS
+                thresholdImage.free();
+                image.free();
+               return reports;
+               
+            } catch (NIVisionException ex) {
+                ex.printStackTrace();
+                return null;
+             } catch (AxisCameraException ex) {
+                    ex.printStackTrace();
+                return null;
+             }
+       }
+       
+       public ParticleAnalysisReport getLargestParticle(int redMin, int redMax, //returns largest particle detected in the current camera image
+               int greenMin, int greenMax, 
+               int blueMin, int blueMax) {
+            ParticleAnalysisReport[] reports = this.getParticles(redMin, redMax, greenMin, greenMax, blueMin, blueMax);
+              int temp =0;
+            double max = 0;
+            for (int i = 0; i < reports.length; i++) {                                // print results
+                    ParticleAnalysisReport r = reports[i];
+                        if(reports[i].particleArea > max){
+                            max = reports[i].particleArea;
+                            temp = i;
+                        }    
+               }
+            return reports[temp];
+       }
+}
